cd48e259ce257f4dded1c9f493a444eb81c7937f
Peter Sulatycke <psulatycke@wowway.com>
2012-11-01 23:07:49 -0500

    Added gl debug info to context

--- src/core/Context.js
+++ src/core/Context.js
@@ -39,6 +39,9 @@ GLOW.Context = (function() {
                                                                               stencil:               this.stencil,
                                                                               premultipliedAlpha:    this.premultipliedAlpha,
                                                                               preserveDrawingBuffer: this.preserveDrawingBuffer } );
+
+                if (DEBUG) this.GL = WebGLDebugUtils.makeDebugContext( this.GL, throwOnGLError, logAndValidate);  // for debug only
+
         	} catch( error ) {
         		GLOW.error( "GLOW.Context.construct: " + error );
         	}
@@ -92,7 +95,26 @@ GLOW.Context = (function() {
             }
     	}
     }
-	
+    // For debug only
+    function logAndValidate(functionName, args) {
+        logGLCalls(functionName, args);
+        validateNoneOfTheArgsAreUndefined (functionName, args);
+    }
+    function logGLCalls(functionName, args) {
+        console.log("gl." + functionName + "(" +
+                        WebGLDebugUtils.glFunctionArgsToString(functionName, args) + ")");
+    }
+    function validateNoneOfTheArgsAreUndefined(functionName, args) {
+        for (var ii = 0; ii < args.length; ++ii) {
+            if (args[ii] === undefined) {
+                console.error("undefined passed to gl." + functionName + "(" +
+                                  WebGLDebugUtils.glFunctionArgsToString(functionName, args) + ")");
+            }
+        }
+    }
+    function throwOnGLError(err, funcName, args) {
+        throw WebGLDebugUtils.glEnumToString(err) + " was caused by call to: " + funcName;
+    }
 	
 	// methods
 	
